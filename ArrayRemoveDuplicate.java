edf74bbb2bcf2cab261a3a5bdb77dff325f0aac3
Ketan <ketanrjoshi04@gmail.com>
2026-07-26 16:39:53 +0530

    new folde

--- ArrayRemoveDuplicate.java
+++ ArrayRemoveDuplicate.java
@@ -2,7 +2,7 @@ public class ArrayRemoveDuplicate {
     public static void main(String[] args) {
       int[] arr = {1, 1, 2, 2, 3, 4, 4, 5};  
       int index=0;
-      int duplicate;
+   
       for(int i=0;i<arr.length-1;i++)
       {
          if(arr[i]==arr[i+1])
